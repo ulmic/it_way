984b2a2fe50361f71ed2213beadee4084b7ee52c
pavel <kalashnikovisme@gmail.com>
2015-09-11 15:06:21 +0300

    fix asets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,4 +6,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( tabs.* web/admin/* ckeditor/* web/* )
+Rails.application.config.assets.precompile += %w( tabs.* web/admin/* ckeditor/* web/* style.css js/* css/* demo/* )
