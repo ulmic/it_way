037a8de0ed976de69b075a2115a345d122c23d27
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-04-19 02:45:27 +0400

    some updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
 
   extend Enumerize
   enumerize :role, in: [ :user, :admin, :author ], default: :user
+  enumerize :activity_line, in: [ :programming, :design, :steel ]
 
   include UserScopes
 
